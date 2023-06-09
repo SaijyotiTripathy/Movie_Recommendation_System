b9d3b599fc09b5f4ec5d011e9621463bbcd4b02b
Saijyoti Tripathy <81572122+SaijyotiTripathy@users.noreply.github.com>
2023-06-09 21:19:45 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -42,25 +42,24 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    #recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    recommended_movie_names = recommend(selected_movie)
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.beta_columns(5)
 
     with col1:
         st.text(recommended_movie_names[0])
-        #st.image(recommended_movie_posters[0])
+        st.image(recommended_movie_posters[0])
     with col2:
         st.text(recommended_movie_names[1])
-        #st.image(recommended_movie_posters[1])
+        st.image(recommended_movie_posters[1])
     with col3:
         st.text(recommended_movie_names[2])
-        #st.image(recommended_movie_posters[2])
+        st.image(recommended_movie_posters[2])
     with col4:
         st.text(recommended_movie_names[3])
-        #st.image(recommended_movie_posters[3])
+        st.image(recommended_movie_posters[3])
     with col5:
         st.text(recommended_movie_names[4])
-        #st.image(recommended_movie_posters[4])
+        st.image(recommended_movie_posters[4])
         
 if st.button('Show Similarity Matrix'):
     st.write(similarity)
